2c71f294e70e486a56c0cd864bde6b58ddecd177
kimiya shabani <kimiyashabani1378@gmail.com>
2024-02-23 12:27:15 +0100

    Random password is being generated correctly and saved in a .txt file

---
+++ main.py
@@ -0,0 +1,77 @@
+import tkinter.messagebox
+from tkinter import *
+import pyperclip
+from random import shuffle, randint, choice
+
+# TODO : 1- UI SETUP
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+canvas = Canvas(window, width=200, height=200)
+
+# photo:
+logo_image = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_image)
+canvas.grid(column=1, row=0)
+
+# labels:
+website_label = Label(window, text="Website:")
+username_label = Label(window, text="Username/Email:")
+password_label = Label(window, text="Password:")
+website_label.grid(row=1, column=0, pady=5)
+username_label.grid(row=2, column=0, pady=5)
+password_label.grid(row=3, column=0, pady=5)
+
+# entries:
+website_entry = Entry(width=50)
+website_entry.focus()
+username_entry = Entry(width=50)
+password_entry = Entry(width=32)
+website_entry.grid(row=1, column=1, columnspan=2)
+username_entry.grid(row=2, column=1, columnspan=2)
+password_entry.grid(row=3, column=1)
+
+
+# TODO : 2- RANDOM PASSWORD GENERATOR FUNCTION
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    random_letters = [choice(letters) for _ in range(randint(1, 8))]
+    random_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    random_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+
+    password = random_letters + random_numbers + random_symbols
+    shuffle(password)
+    password = "".join(password)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
+# TODO : 3- SAVE PASSWORD
+def add_to_passwords():
+    website = website_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
+    if len(website) == 0 or len(username) == 0 or len(password) == 0:
+        tkinter.messagebox.showerror("Error", "All files are required")
+    else:
+        is_ok = tkinter.messagebox.askokcancel(title=website,
+                                               message=f"These are the details entered: \nEmail: {username} "
+                                                       f"\nPassword: {password} \nIs it ok to save?")
+        if is_ok:
+            with open("password.txt", "a") as passwords:
+                passwords.write(f"{website},{username},{password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+
+
+# buttons:
+generate_password_button = Button(text="Generate password", command=generate_password, width=14)
+add_button = Button(text="Add", width=40, command=add_to_passwords)
+generate_password_button.grid(row=3, column=2)
+add_button.grid(row=5, column=1, columnspan=2)
+
+window.mainloop()
