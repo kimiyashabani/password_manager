a850b7e40dfde12ffddbb39c8039e42d2dc130dc
kimiya shabani <kimiyashabani1378@gmail.com>
2024-02-23 14:47:13 +0100

    File format changed from .txt to .json in order to handle data better.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter.messagebox
 from tkinter import *
 import pyperclip
 from random import shuffle, randint, choice
+import json
 
 # TODO : 1- UI SETUP
 window = Tk()
@@ -23,11 +24,11 @@ username_label.grid(row=2, column=0, pady=5)
 password_label.grid(row=3, column=0, pady=5)
 
 # entries:
-website_entry = Entry(width=50)
+website_entry = Entry(width=32)
 website_entry.focus()
 username_entry = Entry(width=50)
 password_entry = Entry(width=32)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1,)
 username_entry.grid(row=2, column=1, columnspan=2)
 password_entry.grid(row=3, column=1)
 
@@ -50,6 +51,7 @@ def generate_password():
     password_entry.insert(0, password)
     pyperclip.copy(password)
 
+
 # TODO : 3- SAVE PASSWORD
 def add_to_passwords():
     website = website_entry.get()
@@ -62,15 +64,33 @@ def add_to_passwords():
                                                message=f"These are the details entered: \nEmail: {username} "
                                                        f"\nPassword: {password} \nIs it ok to save?")
         if is_ok:
-            with open("password.txt", "a") as passwords:
-                passwords.write(f"{website},{username},{password}\n")
+            new_password = {
+                website: {
+                    "username": username,
+                    "password": password
+                }
+            }
+            with open("passwords.json", "w") as passwords:
+                json.dump(new_password, passwords, indent= 4)
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
-
+def calling_search_website():
+    website_name = website_entry.get()
+    search_website(website_name)
+def search_website(website_name):
+    with open("passwords.json", "r") as passwords:
+        passwords_data = json.load(passwords)
+        try:
+            data = passwords_data[website_name]
+            tkinter.messagebox.showinfo("Website found", message=f"username: {data['username']}\npassword: {data['password']}")
+        except:
+            tkinter.messagebox.showerror("website not found", message="You do not have an account in this website")
 
 # buttons:
+load_button = Button(text="Search", command=calling_search_website, width=14)
 generate_password_button = Button(text="Generate password", command=generate_password, width=14)
 add_button = Button(text="Add", width=40, command=add_to_passwords)
+load_button.grid(row=1, column=2 , columnspan=2)
 generate_password_button.grid(row=3, column=2)
 add_button.grid(row=5, column=1, columnspan=2)
 
